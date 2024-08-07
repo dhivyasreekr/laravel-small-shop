b9c5eb19061874715dad8be2f9a50e759f072adc
dhivikutty <dhivyasreekr@gmail.com>
2024-08-07 19:26:06 +0530

    controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,77 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
+
+use App\Models\Product;
+use App\Models\Category;
+
+use Illuminate\Database\Eloquent\ModelFoundException;
+
 class HomeController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $categories = Category::all();
+        $query = Product::query();
+
+        //Appply filters based on the request
+        if($request->has('category') && $request->category != 'All') {
+            $query->where('category_id', $request->category);
+        }
+
+        if($request->has('search')) {
+            $searchTerm = $request->input('search');
+            $query->where('name', 'like', "%(searchTerm)%");
+        }
+
+        $data = [
+            'product' =>$query->get(),
+            'categories' => $categories
+        ];
+
+        return view('frontend/home', $data);
+    }
+
+    public function show($id)
+    {
+        try {
+            $product = Product::findOfFail($id);
+            return view('frontend.show', compact('product'));
+        } catch(ModelNotFoundException $e) {
+            // Handle the case where the product id not found
+            return redirect() ->route('products.index')->with('error', 'Product not found');
+        }
+    }
+
+    public function sendEmailManually()
+    {
+        $recipient = "dhivyasreekr@gmail.com";  // Replace with actual recipient's email
+        $subject = "Custom subject";
+        $body = "This is the body of the email. You can include HTML here id needed";
+
+        Mail::raw($body, function(Message $message) use ($recipient, $subject) {
+            $message->to($recipient);
+            $message->subject($subject);
+            // Ypou can add attachments or other options here if needed
+        });
+
+        return "Email sent successfully";
+    }
+
+    // public function login(Request $request)
+    // {
+    //     return view('frontend/login');
+    // }
+
+    // public function register(Request $request)
+    // {
+    //     return view('frontend/register');
+    // }
+
+    // public function forget_password(Request $request)
+    // {
+    //     return view('frontend/forget_password');
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,72 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'login'])->name('home.login');
 
+Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
+
+Route::get('/profile',[AuthController::class, 'profile'])->name('home.profile');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
 Route::get('/register', [AuthController::class, 'register'])->name('home.register');
 
 Route::get('/forget_password', [AuthController::class, 'forget_password'])->name('home.forget_password');
 
+Route::post('/store', [AuthController::class, 'store'])->name('home.store');
+
+Route::get('/order', [AuthController::class, 'order'])->name('home.order');
+
+Route::get('/role', [AuthController::class, 'role'])->name('home.role');
+
+Route::get('/category', [AuthController::class, 'category'])->name('home.category');
+
+
+use App\Http\Controllers\HomeController;
+Route::get('/',[HomeController::class, 'index'])->name('home.index');
+
+//Product detail page
+Route::get('/products/{id}', [HomeController::class, 'show'])->name('product.show');
+
+Route::get('/sendEmailManually',[HomeController::class, 'sendEmailManually'])->name('home.sendEmailManually');
+
+use App\Http\Controllers\CartController;
+// Cart listing page
+Route::get('/cart',[CartController::class,'index'])->name('cart.index');
+
+// Add to Cart
+Route::post('/cart/add_to_cart', [CartController::class, 'add_to_cart'])->name('cart.add_to_cart');
+
+// Increase quantity route
+Route::post('/cart/increase/{id}', [CartController::class, 'increaseQuantity'])->name('cart.increase');
+
+// Decrease quantity route
+Route::post('/cart/decrease/{id}', [CartController::class, 'decreaseQuantity'])->name('cart.decrease');
+
+// Remove item from cart
+Route::delete('/cart/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
+
+// Checkout route (if you have a checkout process)
+// Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+
+// Clear cart route
+Route::post('/cart/clear', [CartController::class, 'clearCart'])->name('cart.clear');
+
+// Route::get('/sendEmailManually',[HomeController::class, 'sendEmailManually'])->name('home.sendEmailManually');
+
+use App\Http\Controllers\OrderController;
+
+Route::get('/orders', [OrderController::class, 'index'])->name('order.index');
+
+// Checkout Route
+Route::post('checkout', [OrderController::class, 'checkout'])->name('checkout');
+
+// Order Confirmation Route
+Route::get('order/confirmation/{order}', [OrderController::class, 'confirmation'])->name('order.confirmation');
+
+// Order history Route
+Route::get('order/history/{id}', [OrderController::class, 'order_history'])->name('order.history');
+
+// Order show Route
+Route::get('/order/{id}', [OrderController::class, 'show'])->name('order.show');
 
 use App\Http\Controllers\InvoiceController;
 
