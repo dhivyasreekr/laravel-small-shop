53e3031b83b415ada6d48a7c52d2cdb1a1fbfa8d
dhivikutty <dhivyasreekr@gmail.com>
2024-08-08 12:00:03 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use App\Http\Controllers\HomeController;
 Route::get('/',[HomeController::class, 'index'])->name('home.index');
 
 //Product detail page
+Route::get('/products', [HomeController::class, 'index'])->name('product.index');
 Route::get('/products/{id}', [HomeController::class, 'show'])->name('product.show');
 
 Route::get('/sendEmailManually',[HomeController::class, 'sendEmailManually'])->name('home.sendEmailManually');
